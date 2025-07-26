fa6c82866a2aa5d70f73c44aa98dfbfd5c248a7b
Tanaya <tanayapawar248@gmail.com>
2025-07-26 14:00:43 +0530

    Second change

--- Demo.java
+++ Demo.java
@@ -1,6 +1,8 @@
 class Demo 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
     System.out.println("Hello github!!!!");
+    System.out.println("This is the second change I'm making :D");
     }
 }
