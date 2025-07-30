4cda1486594fea8d56b5cf8132ce3396791229d3
Tanaya <tanayapawar248@gmail.com>
2025-07-30 18:12:30 +0530

    Demo java file created

--- Demo.java
+++ Demo.java
@@ -1,8 +1,7 @@
-class Demo 
+class Demo
 {
     public static void main(String[] args) 
     {
-    System.out.println("Hello github!!!!");
-    System.out.println("This is the second change I'm making :D");
+        System.out.println("Hello github :D");
     }
 }
